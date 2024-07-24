04d0a4a3d2d49bd0dca349dcf904673d94f1c1a8
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2024-07-24 06:55:31 -0500

    Update estructura-urls.py

--- estructura-urls.py
+++ estructura-urls.py
@@ -1,1 +1,17 @@
+urls = [
+    {"url": "URL_1", "titulo": "Amenazas en San Luis Antioquia por año"},
+    {"url": "URL_2", "titulo": "Delitos sexuales en San Luis Antioquia por año"},
+    {"url": "URL_3", "titulo": "Extorsión en San Luis Antioquia por año"},
+    {"url": "URL_4", "titulo": "Hurtos al comercio en San Luis"},
+    {"url": "URL_5", "titulo": "Hurto a personas en San Luis"},
+    {"url": "URL_6", "titulo": "Hurtos a residencias en San Luis"},
+    {"url": "URL_7", "titulo": "Hurtos a vehículos en San Luis"},
+    {"url": "URL_8", "titulo": "Lesiones accidente transito San Luis"},
+    {"url": "URL_9", "titulo": "Lesiones personales en San Luis"},
+    {"url": "URL_10", "titulo": "Piratería terrestre en San Luis"},
+    {"url": "URL_11", "titulo": "Homicidios en San Luis"},
+    {"url": "URL_12", "titulo": "Secuestros en San Luis"},
+    {"url": "URL_13", "titulo": "Violencia intrafamiliar en San Luis"},
+    {"url": "URL_CONTRATOS", "titulo": "Contratos"}
+]
 
